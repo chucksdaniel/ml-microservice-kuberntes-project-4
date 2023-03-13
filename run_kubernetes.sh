079087670f1b1d3d6928672fa98e44e55f124117
chucks <iamdanielchukwuma@gmail.com>
2023-03-13 03:36:31 +0100

    Deploy to kubernetes cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="danielchucks/flaslpredictml:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mlpredict --image=$dockerpath --port=80 --labels app=flaskmlpredictk8s
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mlpredict 8000:5000
+
 
